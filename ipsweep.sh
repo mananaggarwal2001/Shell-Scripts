2621e7fab9d935585916a218a8d59fa5159346f1
hackerman <hackerman@kali>
2021-08-24 06:55:17 +0530

    Ipsweep 3

--- ipsweep.sh
+++ ipsweep.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash # this decoration is used for knowing that we are running the bash script. 
 
 if [ "$1" == "" ]   # if syntax introduction 
 then
@@ -15,3 +15,6 @@ fi
 # is used to end the process of the if statement.
 # $ is used for using the variable in the string in the shell
 # & is used just to speed up the things in the script.
+# if we write the ; instead of & in the 
+# (ping -c 1 $1.$ip | grep "64 bytes from" | cut -d " " -f 4 | tr -d ":" &) then the command will run slow and generally looks like our computer is hang 
+# because of using the ; instead of & 
